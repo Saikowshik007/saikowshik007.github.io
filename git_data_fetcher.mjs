5ea63b526e21c570a95328f9d93050541be5f03e
Sai Kowshik Ananthula <ananthulasaikowshik@gmail.com>
2025-06-09 19:05:16 -0500

    removed header and footers

--- git_data_fetcher.mjs
+++ git_data_fetcher.mjs
@@ -5,7 +5,7 @@ import dotenv from "dotenv";
 dotenv.config();
 
 const openSource = {
-  githubConvertedToken: "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",
+  githubConvertedToken: "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",
   githubUserName: "Saikowshik007",
 };
 
